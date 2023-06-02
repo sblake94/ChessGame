d1e0492ce6cde95fee207063853e3434066decdb
Sam Blake <sam_blake94@hotmail.co.uk>
2023-06-02 23:16:50 +0100

    Created TileViewModel
    To reflect the TileView files created in the previous commit

---
+++ WpfUI/ViewModels/TileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Library.Models;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WpfUI.ViewModels;
+
+public class TileViewModel 
+    : ViewModelBase<TileViewModel>
+    , IEquatable<TileModel>
+{
+    public TileModel TileModel { get; }
+    public int Row => TileModel.yPos;
+    public int Column => TileModel.xPos;
+
+    public TileViewModel(TileModel tileModel)
+        : base()
+    {
+        TileModel = tileModel ?? throw new ArgumentNullException(nameof(tileModel));
+    }
+
+
+    public bool Equals(TileModel? other)
+    {
+        return TileModel.Equals(other);
+    }
+}
