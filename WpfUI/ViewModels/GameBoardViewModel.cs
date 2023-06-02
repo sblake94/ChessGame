453c78abbc26c35fbb5bc02a67e08bb6efab274a
Sam Blake <sam_blake94@hotmail.co.uk>
2023-06-02 23:17:59 +0100

    Updating GameBoardViewModel
    Hooking up new behaviours in GameStateEngineService.
    
    
    This probably should have been included in a previous commit, sorry!

--- WpfUI/ViewModels/GameBoardViewModel.cs
+++ WpfUI/ViewModels/GameBoardViewModel.cs
@@ -1,5 +1,8 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using System.Diagnostics;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Input;
+using Library.Models;
+using Library.Services;
+using System;
 using System.Windows.Input;
 
 namespace WpfUI.ViewModels;
@@ -7,16 +10,22 @@ namespace WpfUI.ViewModels;
 public partial class GameBoardViewModel 
     : ViewModelBase<GameBoardViewModel>
 {
+    private readonly IGameStateEngineService _gameStateEngineService;
+
     public ICommand OnTileClicked { get; }
 
     public GameBoardViewModel()
     {
         OnTileClicked = new RelayCommand<string>(Handle_OnTileClicked);
+
+        // TODO: This should be done via constructor injection, but it works for now
+        _gameStateEngineService = Ioc.Default.GetRequiredService<IGameStateEngineService>();
     }
 
     [RelayCommand]
-    private void Handle_OnTileClicked(string tileID)
+    private void Handle_OnTileClicked(string? tileID)
     {
-        Debug.WriteLine($"Tile Clicked: {tileID}");
+        TileModel clickedTile = TileModel.CreateFromTileID(tileID);
+        _gameStateEngineService.SelectTile(clickedTile);        
     }
 }
