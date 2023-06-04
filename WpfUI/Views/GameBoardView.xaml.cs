92b8ad2de5c2c99b1f4339477e806a4c8e7cbddb
Sam Blake <sam_blake94@hotmail.co.uk>
2023-06-04 01:08:01 +0100

    Removing a misplaced method

--- WpfUI/Views/GameBoardView.xaml.cs
+++ WpfUI/Views/GameBoardView.xaml.cs
@@ -4,25 +4,19 @@ using System.Windows.Controls;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using WpfUI.ViewModels;
 
-namespace WpfUI.Views
+namespace WpfUI.Views;
+
+/// <summary>
+/// Interaction logic for GameBoardView.xaml
+/// </summary>
+public partial class GameBoardView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for GameBoardView.xaml
-    /// </summary>
-    public partial class GameBoardView : UserControl
+    GameBoardViewModel ViewModel { get; set; }
+
+    public GameBoardView()
     {
-        GameBoardViewModel ViewModel { get; set; }
+        InitializeComponent();
 
-        public GameBoardView()
-        {
-            InitializeComponent();
-
-            DataContext = ViewModel = new GameBoardViewModel();
-        }
-
-        private void TileView_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-
-        }
+        DataContext = ViewModel = new GameBoardViewModel();
     }
 }
